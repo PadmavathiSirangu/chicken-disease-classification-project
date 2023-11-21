3d38cb65240214848b273dff44253fd907d3cd4b
Padmavathi <sirangu.padmavathi@capgemini.com>
2023-11-21 20:19:29 +0530

    training added

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from Chicken_disease_classifier import logger
 from Chicken_disease_classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from Chicken_disease_classifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from Chicken_disease_classifier.pipeline.stage_03_training import ModelTrainingPipeline
+
+
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -25,6 +28,17 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+STAGE_NAME = "Training"
+try:
+   logger.info(f"******************")
+   logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<<")
+   obj = ModelTrainingPipeline()
+   obj.main()
+   logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\nx========x")
+except Exception as e:
+        logger.exception(e)
+        raise e  
 
 
 
